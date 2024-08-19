82004c02fc2425b6789bf9ce187f534bf3e18377
Saim Shahzad <saimshehzad2030@gmail.com>
2024-08-20 02:07:01 +0500

    switched to supabase

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+})
 //it is a good practice to not create the instance of this prisma client again and again so  we create it only one time
 export default prisma
